72da7d6c94f91b66f2d5afdfae0d5f97c066ea93
wheelsbot7 <o.wertzberger@google.com>
2024-11-04 09:26:27 -0500

    added color borders

--- src/content/config.ts
+++ src/content/config.ts
@@ -9,6 +9,7 @@ const blogCollection = defineCollection({
       description: z.string().max(160),
       pubDate: z.date(),
       updatedDate: z.date().optional(),
+      color: z.string(),
     }),
 });
 
